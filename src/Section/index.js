f1a44247641550b258fe426782eef5aa5335b9f7
Magdalena Checinski <madzix.p730@gmail.com>
2020-08-07 18:48:16 +0200

    Added space before span

--- src/Section/index.js
+++ src/Section/index.js
@@ -5,7 +5,7 @@ const Section = () => (
   <section className="section">
     <p className="section__paragraph">Kursy walut pochodzą ze strony : <a className="section__link"
       href="https://internetowykantor.pl/kursy-walut/" target="_blank" rel="noreferrer noopener">
-      "internetowykantor.pl"</a> z dnia 7 lipca 2020<span className="link__span">(otworzy się w nowej karcie)</span></p>
+      "internetowykantor.pl"</a> z dnia 7 lipca 2020<span className="link__span"> (otworzy się w nowej karcie)</span></p>
   </section>
 );
 
