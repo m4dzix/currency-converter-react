e17defe84ad9e6ce2b8657904d9881f53d0d883d
Magdalena Checinski <madzix.p730@gmail.com>
2020-08-15 14:24:08 +0200

    Added function for calculation currency value

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from './Form';
 import Header from './Header';
 import Section from './Section';
 import Main from './Main'
 import Wrapper from './Wrapper';
 import Body from './Body';
+import currencies from "./Form/currencies";
+
 
 
 function App() {
+
+  const [result, setResult] = useState();
+  const calculateResult = (currencyHave, currencyWant, amount) => {
+
+    const currencyHaveRate = currencies.find
+      (({ shortName }) => shortName === currencyHave).rate;
+
+    const currencyWantRate = currencies.find
+      (({ shortName }) => shortName === currencyWant).rate;
+
+    setResult({
+      sourceAmount: +amount,
+      currencyHave,
+      targetAmount: (amount * currencyHaveRate)/currencyWantRate,
+      currencyWant,
+  });
+  };
+  
   return (
   <Body>
     <Wrapper>
       <Header title= "kalkulator walutowy"/>
         <Main>
-          <Form/>
+        <Form
+           result = {result}
+           calculateResult = {calculateResult}
+        />
           <Section/>
         </Main>
     </Wrapper>
