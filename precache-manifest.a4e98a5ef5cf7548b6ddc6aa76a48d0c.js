4860d8119fbb2326e4c86af9d140045b7831a953
Magdalena Checinski <madzix.p730@gmail.com>
2020-08-15 16:23:25 +0200

    Updates

--- precache-manifest.a4e98a5ef5cf7548b6ddc6aa76a48d0c.js
+++ precache-manifest.a4e98a5ef5cf7548b6ddc6aa76a48d0c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "93595b8a0547748378570643a5584b75",
+    "revision": "9ad54fc3e8ccf26ae8a00176dd4f8167",
     "url": "/cantor-react/index.html"
   },
   {
-    "revision": "7bd1762049e5dee45db3",
-    "url": "/cantor-react/static/css/main.c6336708.chunk.css"
+    "revision": "3b035199a6569dffe278",
+    "url": "/cantor-react/static/css/main.e82f21a3.chunk.css"
   },
   {
     "revision": "9c4b315e06b17e1ae50e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cantor-react/static/js/2.6914f83e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7bd1762049e5dee45db3",
-    "url": "/cantor-react/static/js/main.488d05bb.chunk.js"
+    "revision": "3b035199a6569dffe278",
+    "url": "/cantor-react/static/js/main.35e11d72.chunk.js"
   },
   {
     "revision": "b975c067eb63bcfd848a",
