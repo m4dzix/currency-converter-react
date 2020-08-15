a5e67fdc5ba821ab37a85e120830c78b61f4108c
Magdalena Checinski <madzix.p730@gmail.com>
2020-08-15 15:43:11 +0200

    Updates

---
+++ precache-manifest.da07ce6fbc49df97f9b3138a2308db32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93595b8a0547748378570643a5584b75",
+    "url": "/cantor-react/index.html"
+  },
+  {
+    "revision": "7bd1762049e5dee45db3",
+    "url": "/cantor-react/static/css/main.c6336708.chunk.css"
+  },
+  {
+    "revision": "9c4b315e06b17e1ae50e",
+    "url": "/cantor-react/static/js/2.6914f83e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cantor-react/static/js/2.6914f83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd1762049e5dee45db3",
+    "url": "/cantor-react/static/js/main.488d05bb.chunk.js"
+  },
+  {
+    "revision": "b975c067eb63bcfd848a",
+    "url": "/cantor-react/static/js/runtime-main.61db47e7.js"
+  }
+]);
