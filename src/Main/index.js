2690d8c5784a30e412710f35629ef80252212d24
m4dzix <m4dzix@users.noreply.github.com>
2020-08-20 20:44:31 +0200

    Moved and set the date and time inside Main

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import './style.css'
+import ShowDate from "./ShowDate"
 
 const Main = ({children}) => (
   <main className="main">
+     <ShowDate/>
     {children}
   </main>
 )
