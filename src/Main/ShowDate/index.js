e2f84b901b081748ae45c59da7ae5f2dad4fe0ec
m4dzix <m4dzix@users.noreply.github.com>
2020-08-20 21:13:05 +0200

    Added date and time format

--- src/Main/ShowDate/index.js
+++ src/Main/ShowDate/index.js
@@ -1,23 +1,34 @@
 import React, {useState, useEffect} from 'react';
-import './style.css'
+import './style.css';
+
+const formatDate = (showDate) => 
+    showDate.toLocaleString(undefined, {
+        weekday:"long",
+        hour:"2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day:"numeric",
+        month:"long",
+    });
+
 
 const ShowDate = () => {
     
-    const [showDate, setShowDate] = useState( new Date().toLocaleString());
+    const [showDate, setShowDate] = useState( new Date() );
 
     useEffect(() => {
         const intervalId = setInterval( () => {
-        setShowDate( showDate => new Date().toLocaleString() )
+        setShowDate(new Date());
     }, 1000);
 
-    return () => {clearInterval(intervalId)
+    return () => {clearInterval(intervalId);
     };
     }, []);
 
 return(
 
 <p className = "form__showDate">
- Aktualna data i godzina: {showDate}
+ Aktualna data i godzina:{formatDate(showDate)}
 </p>
 );
 }
