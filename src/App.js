33a3fa4b340b45645fa358087d2340fa18edae59
m4dzix <m4dzix@users.noreply.github.com>
2020-09-02 22:49:10 +0200

    Using API to get currencies

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
-import React, {useState} from 'react';
-import Form from './Form';
-import Header from './Header';
-import Section from './Section';
-import Main from './Main'
-import Wrapper from './Wrapper';
-import currencies from "./Form/currencies";
-
-
-
+import React, { useState } from "react";
+import Form from "./Form";
+import Header from "./Header";
+import Section from "./Section";
+import Main from "./Main";
+import Wrapper from "./Wrapper";
+import { useRates } from "./useRates";
 function App() {
-
   const [result, setResult] = useState();
+  const { rates } = useRates();
   const calculateResult = (currencyHave, currencyWant, amount) => {
-
-    const currencyHaveRate = currencies.find
-      (({ shortName }) => shortName === currencyHave).rate;
-
-    const currencyWantRate = currencies.find
-      (({ shortName }) => shortName === currencyWant).rate;
-
     setResult({
       sourceAmount: +amount,
       currencyHave,
-      targetAmount: (amount * currencyHaveRate)/currencyWantRate,
+      targetAmount: (amount * rates[currencyWant]) / rates[currencyHave],
       currencyWant,
-  });
+    });
   };
-  
+
   return (
     <Wrapper>
-      <Header title= "kalkulator walutowy"/>
-        <Main>
-        <Form
-           result = {result}
-           calculateResult = {calculateResult}
-        />
-          <Section/>
-        </Main>
+      <Header title="kalkulator walutowy" />
+      <Main>
+        <Form result={result} calculateResult={calculateResult} />
+        <Section />
+      </Main>
     </Wrapper>
   );
-};
+}
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -9,14 +9,17 @@ import {
   Button,
   Fieldset,
   Legend,
+  Information,
 } from "./styled";
-import currencies from "./currencies";
 import ShowResult from "./ShowResult";
+import { useRates } from "../useRates";
 
 const Form = ({ calculateResult, result }) => {
-  const [currencyHave, setCurrencyHave] = useState(currencies[0].shortName);
-  const [currencyWant, setCurrencyWant] = useState(currencies[1].shortName);
+  const { rates } = useRates();
+  const currencyApiArray = Object.keys(useRates().rates);
   const [amount, setAmount] = useState("");
+  const [currencyHave, setCurrencyHave] = useState(currencyApiArray[1]);
+  const [currencyWant, setCurrencyWant] = useState(currencyApiArray[1]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -27,60 +30,71 @@ const Form = ({ calculateResult, result }) => {
     <StyledForm onSubmit={onSubmit}>
       <Fieldset>
         <Legend>Wymiana waluty</Legend>
-        <Paragraph>Pole oznaczone * musi zostać wypełnione</Paragraph>
-        <p>
-          <label>
-            <LabelText>
-              wymiana z:
-              <Span>(wybierz walutę)</Span>
-            </LabelText>
-            <Select
-              value={currencyHave}
-              onChange={({ target }) => setCurrencyHave(target.value)}
-              name="currencyHave"
-            >
-              {currencies.map((currency) => (
-                <option key={currency.shortName} value={currency.shortName}>
-                  {currency.shortName}
-                </option>
-              ))}
-            </Select>
-          </label>
-        </p>
-        <p>
-          <label>
-            <LabelText>
-              wymiana na:
-              <Span>(wybierz walutę)</Span>
-            </LabelText>
-            <Select
-              value={currencyWant}
-              onChange={({ target }) => setCurrencyWant(target.value)}
-              name="currencyWant"
-            >
-              {currencies.map((currency) => (
-                <option key={currency.shortName} value={currency.shortName}>
-                  {currency.shortName}
-                </option>
-              ))}
-            </Select>
-          </label>
-        </p>
-        <p>
-          <label>
-            <LabelText>Podaj kwotę*: </LabelText>
-            <Field
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-              name="value"
-              type="number"
-              required
-              step="0.01"
-            />
-          </label>
-        </p>
-        <Button>przelicz</Button>
-        <ShowResult result={result} />
+
+        {rates ? (
+          <>
+            <Paragraph>Pole oznaczone * musi zostać wypełnione</Paragraph>
+            <p>
+              <label>
+                <LabelText>
+                  wymiana z:
+                  <Span>(wybierz walutę)</Span>
+                </LabelText>
+                <Select
+                  value={currencyHave}
+                  onChange={({ target }) => setCurrencyHave(target.value)}
+                  name="currencyHave"
+                >
+                  {currencyApiArray.map((currencyApiArray) => (
+                    <option key={currencyApiArray} value={currencyApiArray}>
+                      {currencyApiArray}
+                    </option>
+                  ))}
+                </Select>
+              </label>
+            </p>
+            <p>
+              <label>
+                <LabelText>
+                  wymiana na:
+                  <Span>(wybierz walutę)</Span>
+                </LabelText>
+                <Select
+                  value={currencyWant}
+                  onChange={({ target }) => setCurrencyWant(target.value)}
+                  name="currencyWant"
+                >
+                  {currencyApiArray.map((currencyApiArray) => (
+                    <option key={currencyApiArray} value={currencyApiArray}>
+                      {currencyApiArray}
+                    </option>
+                  ))}
+                </Select>
+              </label>
+            </p>
+            <p>
+              <label>
+                <LabelText>Podaj kwotę*: </LabelText>
+                <Field
+                  value={amount}
+                  onChange={({ target }) => setAmount(target.value)}
+                  name="value"
+                  type="number"
+                  required
+                  step="0.01"
+                />
+              </label>
+            </p>
+            <Button>przelicz</Button>
+            <ShowResult result={result} />
+          </>
+        ) : (
+          <Information>
+            {rates === "error"
+              ? "Przepraszamy wystapił błąd, chyba masz problem z internetem, jeśli nie - spróbój później"
+              : "Poczekaj chwilkę, ładujemy teraz dane z Europejskiego Banku Centralnego "}
+          </Information>
+        )}
       </Fieldset>
     </StyledForm>
   );
