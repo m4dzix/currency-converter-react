56f7c59311ce60927b57952bff0f962829019342
m4dzix <m4dzix@users.noreply.github.com>
2020-09-02 22:47:23 +0200

    Created custom hook to using API

---
+++ src/useRates.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export const useRates = () => {
+  const [data, setData] = useState({
+    rates: "",
+    date: "",
+  });
+
+  useEffect(() => {
+    const getApi = () => {
+      const currenciesAPI = "https://api.exchangeratesapi.io/latest?base=PLN";
+
+      fetch(currenciesAPI)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response;
+        })
+        .then((response) => response.json())
+        .then((currenciesAPI) => {
+          const ratesAPI = currenciesAPI.rates;
+          const dateAPI = currenciesAPI.date;
+          setData({
+            rates: ratesAPI,
+            date: dateAPI,
+          });
+        })
+        .catch((error) => console.error(error));
+    };
+    setTimeout(getApi, 1000);
+  }, []);
+  return data;
+};
