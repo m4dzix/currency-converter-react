5e0f94cdef9aa54a469e5b5741a417fc2a48a9ea
m4dzix <madzix.p730@gmail.com>
2020-09-11 13:49:42 +0200

    Moved styles from index.css to global style

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,13 +4,24 @@ export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
   font-family: "Roboto", sans-serif;
-}
-
+};
 *,
 ::after,
 ::before {
   box-sizing: inherit;
-}
+};
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+};
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+};
 #root {
   background-image: url(https://cdn.pixabay.com/photo/2020/06/25/06/32/money-5338733__340.jpg);
   background-size: cover;
